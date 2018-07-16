33b67d504c8aa7d916023a2c9d22caec0fdbe4f4
Joe Velez <joe.velez@cbsinteractive.com>
2018-07-16 14:49:21 -0700

    test commit

--- server/main.js
+++ server/main.js
@@ -50,5 +50,6 @@ app.get('/', (req, res) => {
 })
 
 const listener = app.listen(3000, () => {
-  console.log('app is running on port 3000')
+  console.log('app is running on port 3000');
+  console.log('it works');
 })
