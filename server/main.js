5dd7468c731a720a8d7059ea4b2d918ffcd979be
Joe Velez <joe.velez@cbsinteractive.com>
2018-07-18 15:15:58 -0700

    adding body-parser, remove initial scrub and only scrub cbsnews, new object response returned with videos and articles separated

--- server/main.js
+++ server/main.js
@@ -1,57 +1,71 @@
 const express = require('express')
 const rp = require('request-promise')
 const cheerio = require('cheerio')
-const app = express()
+const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('*', (req, res) => {
   
-  const options = {
-    uri: req.query.url,
-    transform: function (body) {
-      return cheerio.load(body);
-    }
+  let scrubHost = 'https://www.cbsnews.com/search/?q=' + encodeURI(req.body.title);
+  
+  if(req.body.provider === 'cbsnews'){
+  
   }
-
-  rp(options)
-    .then(($) => {
-      const title = $('span#series_title').html()
-
-      const opt = {
-        uri: 'https://www.cbsnews.com/search/?q=' + encodeURI(title),
-        transform: function (body) {
-          return cheerio.load(body);
-        }
+  
+  let options = {
+    uri: scrubHost,
+      transform: function(body){
+        return cheerio.load(body);
       }
+  };
+  
+  rp(options).then( ($) => {
+   
+	  const resultArr = $('.result-list ul.items li');
+	  
+	  const resp = {
+	  	articles: [],
+	  	videos: []
+	  };
+	
+	  resultArr.each(function() {
+		
+		  let isVideo = $(this).find('.play-video').length > 0;
+	  	
+		  let temp = {
+			  title: $(this).find('a h3.title').text(),
+			  href: 'https://www.cbsnews.com' + $(this).find('a').attr('href'),
+			  thumb: $(this).find('a figure.media-figure img').attr('src'),
+			  body: $(this).find('div.media-body p.dek ').html(),
+			  isVideo: isVideo
+		  };
+		
+		  if(isVideo){
+		  	resp.videos.push(temp);
+		  }else{
+		  	resp.articles.push(temp);
+		  }
+		  
+	  });
+	  
+	  res.json(resp);
+	  
+  }).catch((err) => {
+  	
+  	res.json({
+		error: err
+	});
+  
+  });
 
-      rp(opt)
-        .then(($) => {
-          const resultArr = $('.result-list ul.items li');
-          const items = [];
-
-          resultArr.each(function() {
-            
-            let temp = {
-              title: $(this).find('a h3.title').text(),
-              href: 'https://www.cbsnews.com' + $(this).find('a').attr('href'),
-              thumb: $(this).find('a figure.media-figure img').attr('src'),
-              body: $(this).find('div.media-body p.dek ').html(),
-              isVideo: $(this).find('.play-video').length > 0
-            }
-
-            items.push(temp)
-          })
-
-          res.json(items)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-})
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('app is running on port 3000');
-})
+});
