c6bbbf3e2d6852dfb9183dcc16bce157bc9e27c9
Joe Velez <joe.velez@cbsinteractive.com>
2018-07-18 15:15:16 -0700

    adding new rule for local

--- chrome/background.js
+++ chrome/background.js
@@ -1,18 +1,28 @@
 // make icon available only if user on show video page
 let rule1 = {
-  conditions: [new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: {hostEquals: 'www.cbs.com'},
-      css: ["span[id='series_title']"]
-    })
-  ],
-  actions: [new chrome.declarativeContent.ShowPageAction()]
-}
+	conditions: [new chrome.declarativeContent.PageStateMatcher({
+		pageUrl: {hostEquals: 'www.cbs.com'},
+		css: ["span[id='series_title']"]
+	})
+	],
+	actions: [new chrome.declarativeContent.ShowPageAction()]
+};
+
+let rule2 = {
+	conditions: [new chrome.declarativeContent.PageStateMatcher({
+		pageUrl: {hostEquals: 'local.cbs.com'},
+		css: ["span[id='series_title']"]
+	})
+	],
+	actions: [new chrome.declarativeContent.ShowPageAction()]
+};
 
 chrome.runtime.onInstalled.addListener(function() {
   console.log('CBS Companion is installed');
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([rule1]);
+	  chrome.declarativeContent.onPageChanged.addRules([rule1]);
+	  chrome.declarativeContent.onPageChanged.addRules([rule2]);
   });
 
   // show notification when user on show video page
